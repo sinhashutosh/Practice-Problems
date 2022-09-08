b99e0cda638be68662b325c13d6e35db094a91ab
Ashutosh <ashu.cse.sln@gmail.com>
2022-09-08 13:50:29 +0530

    String Compare with User Input

--- StringCompare.java
+++ StringCompare.java
@@ -1,8 +1,8 @@
 package org.example;
-
+import java.util.Scanner;
 public class StringCompare {
     public static void main(String[] args) {
-        String string1 = new String("Ashutosh12");
+        /*String string1 = new String("Ashutosh12");
         String string2 = new String("Singh");
         String string3 = new String("INDIA");
         String string4 = new String("INDIA");
@@ -15,14 +15,16 @@ public class StringCompare {
             System.out.println("Both String Are Equal: ");
         }else {
             System.out.println("Both String Are NOT Equal: ");
+        }*/
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First String");
+        String s1 = sc.next();
+        System.out.println("Enter Second String");
+        String s2 = sc.next();
+        if (s1.equals(s2)) {
+            System.out.println("Both String are Equal ");
+        } else {
+            System.out.println("Both String are NOT Equal ");
         }
-
-
-        String s1 = "HELLO";
-        String s2 = "HELLO";
-
-        System.out.println(s1 == s2);
-
-        System.out.println(s1.equals(s2));
     }
 }
