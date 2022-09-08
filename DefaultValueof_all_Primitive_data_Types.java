c70ea1428f572af1387ee047763f9bdaa287cf42
Ashutosh <ashu.cse.sln@gmail.com>
2022-09-08 13:54:35 +0530

    Default value of Primitive Data type generated

---
+++ DefaultValueof_all_Primitive_data_Types.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class DefaultValueof_all_Primitive_data_Types {
+    static char ch;
+    static int a;
+    static long b;
+    static float c;
+    static double d;
+    static  short e;
+    static boolean f;
+    static byte g;
+    public static void main(String[] args) {
+        /**static char ch;
+         static int a;
+         static long b;
+         static float c;
+         static double d;
+         static  short e;
+         static boolean f;
+         static byte g;*/
+        System.out.println("Default value of char = "+ch);
+        System.out.println("Default value of int = "+a);
+        System.out.println("Default value of long = "+b);
+        System.out.println("Default value of float = "+c);
+        System.out.println("Default value of double = " +d);
+        System.out.println("Default value of short = " +e);
+        System.out.println("Default value of boolean = "+f);
+        System.out.println("Default value of byte = "+g);
+
+    }
+
+}
