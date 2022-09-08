2d31b3a1df12e5cbc6b6f5b666df698bb4bf0994
Ashutosh <ashu.cse.sln@gmail.com>
2022-09-08 19:29:12 +0530

    Sum of Natural number using for()

---
+++ NaturalNumberSum.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class NaturalNumberSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The value of n : ");
+        int sum=0;
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++) {
+            sum+=i;
+        }
+        System.out.println("Sum of "+n+" natural number =  "+sum);
+    }
+}
