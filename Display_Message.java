df055d8d2ad6c35e8fd691c1f05dd514d13784db
Ashutosh <ashu.cse.sln@gmail.com>
2022-09-08 13:23:17 +0530

    Problem of Display Message is solve

---
+++ Display_Message.java
@@ -0,0 +1,6 @@
+package org.example;
+public class Display_Message {
+    public static void main(String[] args) {
+        System.out.println("\n\nBetter than the entire world, is our Hindustan,\n");
+    }
+}
