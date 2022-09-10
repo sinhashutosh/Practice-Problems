27581ad33c7d493772fcc3f8b3a764bf9f059f74
Ashutosh <ashu.cse.sln@gmail.com>
2022-09-10 23:42:33 +0530

    Error Solved

--- DuplicateElement.java
+++ DuplicateElement.java
@@ -1,2 +1,35 @@
-package org.example;public class DuplicateElement {
+package org.example;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DuplicateElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the sizen of an array");
+        int size = sc.nextInt();
+        int[] array = new int[size];
+        System.out.println("Enter the element of an array");
+        for (int i = 0; i < size; i++) {
+            array[i] = sc.nextInt();
+        }
+        System.out.println("Enter Array Element are ...");
+        System.out.println(Arrays.toString(array));
+        int count =0;
+        for (int i = 0; i < size; i++) {
+            for (int j = i+1; j < size; j++) {
+                if (array[i]==array[j])
+                {
+                    count=1;
+                    System.out.print(array[i]+"  is an duplicate element");
+                }
+
+            }
+
+        }
+        if (count ==0) {
+            System.out.println("There is no duplicate element");
+        }
+    }
 }
+
