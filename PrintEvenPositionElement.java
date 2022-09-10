e7d5081b06b8aafd3f20cbd2fed89fab76deb6ec
Ashutosh <ashu.cse.sln@gmail.com>
2022-09-10 21:57:16 +0530

    Java Program to print the elements of an array present on an even position

---
+++ PrintEvenPositionElement.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PrintEvenPositionElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of an Array");
+        int size = sc.nextInt();
+        int[] array = new int[size];
+        System.out.println("Enter the Element of An Array...");
+        for (int i = 0; i < size; i++) {
+            array[i]=sc.nextInt();
+        }
+        System.out.println("Entered Array Are...");
+        System.out.println(Arrays.toString(array));
+        System.out.println("Element of Even Position Array are..");
+        for (int i = 1; i < size; i+=2) {
+            System.out.print(array[i]+" ");
+        }
+    }
+}
